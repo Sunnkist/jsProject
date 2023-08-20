3959b023c9e532fb980e43c767e1bffb468acac2
Suna Lee <sunna778@gmail.com>
2023-08-20 18:04:53 -0400

    update

--- js/background.js
+++ js/background.js
@@ -1,9 +1,11 @@
-const images = ["img1.jpg", "img2.jpg", "img3.jpeg", "img4.jpg"];
+const images = ["0.jpg", "1.jpg", "2.jpeg", "3.jpg"];
 
 const chosenImage = images[Math.floor(Math.random() * images.length)];
 
-const bgImage = document.createElement("img");
-
+//const bgImage = document.createElement("img");
+const bgImage = document.querySelector("img");
+//bgImage.style.cssText = 'width:100%;height:80%'
 bgImage.src = `img/${chosenImage}`;
 
-document.body.appendChild(bgImage);
+//document.body.appendChild(bgImage);
+//bgImage.addEventListener();
